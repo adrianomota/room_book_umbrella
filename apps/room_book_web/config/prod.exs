682cc2fc5074e523f8e94f818a2cb7929dd210a6
Adriano Mota <adrianomota@adriano-2.local>
2018-08-26 14:25:17 -0300

    ädicionando arquivos para deploy

--- apps/room_book_web/config/prod.exs
+++ apps/room_book_web/config/prod.exs
@@ -15,8 +15,10 @@ use Mix.Config
 # which you typically run after static files are built.
 config :room_book_web, RoomBookWeb.Endpoint,
   load_from_system_env: true,
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  url: [scheme: "https", host: "https://roombook-app.herokuapp.com/", port: 443],
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
 # ## SSL Support
 #
