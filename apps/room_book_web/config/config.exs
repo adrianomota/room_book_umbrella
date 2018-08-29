6ce1bb6f3594b5031953d9dec8258573461d674c
Adriano Mota <adrianomota@adriano-2.local>
2018-08-29 18:27:29 -0300

    adjust configurations

--- apps/room_book_web/config/config.exs
+++ apps/room_book_web/config/config.exs
@@ -4,22 +4,20 @@
 # This configuration file is loaded before any dependency and
 # is restricted to this project.
 use Mix.Config
-# General application configuration
-config :room_book_web,
-  namespace: RoomBookWeb
 
 # Configures the endpoint
 config :room_book_web, RoomBookWeb.Endpoint,
   url: [host: "localhost"],
+  secret_key_base: "9cTL1kK+LrgnsSwXFN59NBtoa4vWhiUMJ8a3Goveous4cwIwZkopzPtuy0TfO6S6H",
   render_errors: [view: RoomBookWeb.ErrorView, accepts: ~w(html json)],
-  pubsub: [name: RoomBookWeb.PubSub, adapter: Phoenix.PubSub.PG2]
+  pubsub: [name: RoomBookWeb.PubSub,
+           adapter: Phoenix.PubSub.PG2]
 
 # Configures Elixir's Logger
 config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   metadata: [:request_id]
 
-config :room_book_web, :generators, context_app: false
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
-# import_config "#{Mix.env}.exs"
+import_config "#{Mix.env}.exs"
