7ad226e27e3bb1529acc70c6ec9b0880257acf49
Adriano Mota <adrianomota@adriano-2.local>
2018-08-29 00:27:06 -0300

    adjust system variable

--- apps/room_book_web/config/prod.exs
+++ apps/room_book_web/config/prod.exs
@@ -16,7 +16,7 @@ config :room_book_web, RoomBookWeb.Endpoint,
   load_from_system_env: true,
   url: [host: "example.com", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json",
-  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # ## SSL Support
 #
